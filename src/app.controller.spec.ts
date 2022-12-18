4c4b4b3f18dccc2bea66658f089a83c431a350b8
CTSI <ctsi@adinet.com.uy>
2022-12-18 19:51:36 -0300

    primera entrega

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hola. Esta es mi primer aplicacion en Nest!"', () => {
+      expect(appController.getHello()).toBe('Hola. Esta es mi primer aplicacion en Nest!');
+    });
+  });
+});
